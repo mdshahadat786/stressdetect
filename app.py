00065b64071a9d94418b5880b1f6da83f94c6536
mdshahadat786 <24mmca023hy@manuu.edu.in>
2026-03-26 16:29:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Title
+st.title("AI Student Stress Detection System (Federated Learning)")
+
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
+
+# Input Section
+st.header("Enter Student Details")
+
+study = st.slider("Study Hours", 0, 10)
+sleep = st.slider("Sleep Hours", 0, 10)
+social = st.slider("Social Media Usage", 0, 8)
+pressure = st.slider("Academic Pressure", 1, 5)
+family = st.slider("Family Support", 1, 5)
+activity = st.slider("Physical Activity", 0, 5)
+screen = st.slider("Screen Time", 0, 10)
+extra = st.slider("Extracurricular Activities", 0, 5)
+financial = st.slider("Financial Stress", 1, 5)
+examfear = st.slider("Exam Fear", 1, 5)
+timemanagement = st.slider("Time Management", 1, 5)
+
+# Button
+if st.button("Predict Stress"):
+
+    # Prepare data
+    data_input = np.array([[study, sleep, social, pressure, family,
+                            activity, screen, extra, financial,
+                            examfear, timemanagement]])
+
+    # Prediction
+    prediction = model.predict(data_input)
+
+    # Stress Score Calculation
+    stress_score = 0
+
+    if sleep < 6: stress_score += 20
+    if study > 7: stress_score += 15
+    if social > 5: stress_score += 10
+    if pressure > 3: stress_score += 20
+    if family < 3: stress_score += 10
+    if activity < 2: stress_score += 10
+    if screen > 7: stress_score += 10
+    if financial > 3: stress_score += 10
+    if examfear > 3: stress_score += 10
+    if timemanagement < 3: stress_score += 10
+
+    # Show Score
+    st.subheader("Stress Score")
+    st.write(stress_score)
+
+    # Stress Level
+    if stress_score <= 30:
+        st.success("Low Stress ")
+    elif stress_score <= 60:
+        st.warning("Moderate Stress ")
+    else:
+        st.error("High Stress ")
+
+    # Model Output
+    if prediction[0] == 1:
+        st.error("Model Prediction: Student is Stressed")
+    else:
+        st.success("Model Prediction: Student is Not Stressed")
+
+    # Suggestions
+    st.subheader("Personalized Suggestions")
+
+    if sleep < 6:
+        st.write("Maintain a proper sleep schedule.")
+    if study > 7:
+        st.write("Take regular breaks during study time.")
+    if social > 5:
+        st.write(" Limit your social media usage.")
+    if pressure > 3:
+        st.write(" Manage academic tasks step by step.")
+    if family < 3:
+        st.write(" Seek support from family or close ones.")
+    if activity < 2:
+        st.write(" Include physical activity in your routine.")
+    if screen > 7:
+        st.write(" Reduce screen time and take breaks.")
+    if financial > 3:
+        st.write("Discuss financial concerns with a trusted person.")
+    if examfear > 3:
+        st.write(" Practice regularly to build confidence.")
+    if timemanagement < 3:
+        st.write("Follow a structured daily schedule.")
+
+    # Extra Message
+    if stress_score <= 30:
+        st.write(" You are managing well. Keep it up!")
+    elif stress_score > 60:
+        st.write("Focus on improving your routine. You can manage this.")
